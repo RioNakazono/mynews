d350cf2893dde335e95f2085b711d8b566481366
Rio <riohime123@gmail.com>
2021-04-07 09:42:25 +0000

    laravel08課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -11,20 +11,20 @@ class ProfileController extends Controller
   {
     return view('admin.profile.create');
   }
-  
-
+    //↑プロフィールを入力する画面を表示
   public function create()
   {
     return redirect('admin/profile/create');
   }
-
+    //↑DBに保存して、再度入力する画面に戻る
   public function edit()
   {
     return view('admin.profile.edit');
   }
-
+    //↑プロフィールを編集する画面を表示
   public function update()
   {
     return redirect('admin/profile/edit');
-  }    //
+  }   
+    //↑DBに保存して、再度編集する画面に戻る
 }
